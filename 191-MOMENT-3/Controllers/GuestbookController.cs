21e40aa61374229ac0c1505942b658008b18e058
DamjanT1988 <Damjan_t@hotmail.com>
2023-02-01 02:55:57 +0100

    1

--- 191-MOMENT-3/Controllers/GuestbookController.cs
+++ 191-MOMENT-3/Controllers/GuestbookController.cs
@@ -1,22 +1,80 @@
-﻿using System;
+﻿using _191_MOMENT_3.Models;
+using _191_MOMENT_3_TEST1.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
-
-using Xamarin.Forms;
 
 namespace _191_MOMENT_3.Controllers
 {
-    public class GuestbookController : ContentPage
+    public class GuestbookController : Controller
     {
-        public GuestbookController()
+        //---DATABASE
+
+        //create db connection (protected)
+        private readonly GuestbookContext _context;
+
+        public GuestbookController(GuestbookContext context)
         {
-            Content = new StackLayout
+            //protected db connection
+            _context = context;
+        }
+
+        
+        //---VIEWS
+
+        //start page
+        //make async, to have more tasks
+        public async Task<IActionResult> Index()
+        {
+            //return list of objects from db (make list by ToList)
+            return View(await _context.guestbook.ToListAsync());
+        }
+
+        //create page
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //---HTTP ACTIONS
+
+        //on POST
+        [HttpPost]
+        //control that the actual form is sent
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(GuestbookModel guestbook)
+        {
+            if (ModelState.IsValid)
             {
-                Children = {
-                    new Label { Text = "Welcome to Xamarin.Forms!" }
-                }
-            };
+                //add to db, adding instance guestbook
+                _context.Add(guestbook);
+                //save changes, async
+                await _context.SaveChangesAsync();
+
+                //redirect user to index page
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        //on POST
+        [HttpPost]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var guestbookItem = await _context.guestbook.FindAsync(Id);
+            if(guestbookItem != null)
+            {
+                _context.guestbook.Remove(guestbookItem);
+                await _context.SaveChangesAsync();
+            }
+
+            //redirect user to index page
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- 191-MOMENT-3/Models/GuestbookModel.cs
+++ 191-MOMENT-3/Models/GuestbookModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
@@ -9,9 +10,13 @@ namespace _191_MOMENT_3.Models
     public class GuestbookModel
     {
         //Properties
+        //ID = auto
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
+        [Required]
         public string? Message { get; set; }
+        //DATE = auto
         public DateTime PostedDate { get; set; } = DateTime.Now;
     }
 }
