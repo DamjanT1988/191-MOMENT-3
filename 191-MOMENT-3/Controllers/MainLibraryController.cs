522f4fe2b37e51d8fb2030fcc3c593018e7b95ea
DamjanT1988 <Damjan_t@hotmail.com>
2023-02-07 20:46:01 +0100

    1

---
+++ 191-MOMENT-3/Controllers/MainLibraryController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using _191_MOMENT_3.Data;
+using _191_MOMENT_3.Models;
+
+namespace _191_MOMENT_3.Controllers
+{
+    public class MainLibraryController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public MainLibraryController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MainLibrary
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.mainLibrary.ToListAsync());
+        }
+
+        // GET: MainLibrary/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.mainLibrary == null)
+            {
+                return NotFound();
+            }
+
+            var libraryModel = await _context.mainLibrary
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (libraryModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(libraryModel);
+        }
+
+        // GET: MainLibrary/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MainLibrary/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Artist,Album,Year,Minutes,PostedDate")] LibraryModel libraryModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(libraryModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libraryModel);
+        }
+
+        // GET: MainLibrary/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.mainLibrary == null)
+            {
+                return NotFound();
+            }
+
+            var libraryModel = await _context.mainLibrary.FindAsync(id);
+            if (libraryModel == null)
+            {
+                return NotFound();
+            }
+            return View(libraryModel);
+        }
+
+        // POST: MainLibrary/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Artist,Album,Year,Minutes,PostedDate")] LibraryModel libraryModel)
+        {
+            if (id != libraryModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(libraryModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LibraryModelExists(libraryModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libraryModel);
+        }
+
+        // GET: MainLibrary/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.mainLibrary == null)
+            {
+                return NotFound();
+            }
+
+            var libraryModel = await _context.mainLibrary
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (libraryModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(libraryModel);
+        }
+
+        // POST: MainLibrary/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.mainLibrary == null)
+            {
+                return Problem("Entity set 'LibraryContext.mainLibrary'  is null.");
+            }
+            var libraryModel = await _context.mainLibrary.FindAsync(id);
+            if (libraryModel != null)
+            {
+                _context.mainLibrary.Remove(libraryModel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LibraryModelExists(int id)
+        {
+          return _context.mainLibrary.Any(e => e.Id == id);
+        }
+    }
+}
--- 191-MOMENT-3/Models/LibraryModel.cs
+++ 191-MOMENT-3/Models/LibraryModel.cs
@@ -12,7 +12,10 @@ namespace _191_MOMENT_3.Models
         //Properties
         //ID = auto, primary key (PK)
         public int Id { get; set; }
-        [Required]
+        //required to fill in to form
+        [Required(ErrorMessage = "Please fill in the artist name!")]
+        //display specific name
+        [Display(Name = "Artist name:")]
         public string? Artist { get; set; }
         [Required]
         public string? Album { get; set; }
@@ -22,5 +25,8 @@ namespace _191_MOMENT_3.Models
         public int? Minutes { get; set; }
         //DATE = auto
         public DateTime PostedDate { get; set; } = DateTime.Now;
+
+        //limits other types: email, phone, min.length etc
+        // [DataType(DataType.EmailAddress)] - validate email
     }
 }
